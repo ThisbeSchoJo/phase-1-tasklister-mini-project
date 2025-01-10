fd1ae9c830fe774c501bb21479642a3067a1e632
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-01-10 09:50:39 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const newTask = document.querySelector("#new-task-description").value // access input value directly
+    buildToDo(newTask) // pass the value to the function
+    form.reset(); //clear the input field
+  })
 });
+
+
+function buildToDo(todo) {
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.textContent = "O"
+  btn.addEventListener("click", handleDelete)
+  p.textContent = todo
+  p.appendChild(btn)
+  // console.log(p)
+  document.querySelector("#tasks").appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
